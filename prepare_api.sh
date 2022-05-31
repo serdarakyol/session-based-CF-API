6b598db55917ca37e5d1160eeedf9608574704b3
serdarakyol <serdarakyol55@outlook.com>
2022-05-31 15:38:20 +0200

    added prepare api script

---
+++ prepare_api.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# create and fill .env file
+touch .env
+cat << EOF > .env
+IS_DEBUG=False
+API_KEY='serdarakyol55@outlook.com'
+EOF
+
+# create virtual env
+virtualenv venv
+source venv/bin/activate
+
+# install requirements.txt
+pip install -r requirements.txt
+
+# create data folder
+current_folder="$(pwd)"
+data_folder=$current_folder"/cf_api/data"
+mkdir $data_folder
+# install files in data
+echo "Downloading collaborative filter data to $data_folder/all_data.pkl"
+gdown https://drive.google.com/uc?id=18KO3wmlaAy1yW9HjZ82GXSXTbD2g1AoQ -O $data_folder"/all_data.pkl"
+echo "Downloading products data to $data_folder/meta.json"
+gdown https://drive.google.com/uc?id=1tQezbs22O_-ZtzOhf6GUAs5noDPRdiFU -O $data_folder"/meta.json"
